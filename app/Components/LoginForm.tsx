588dabf7cea9b52aa856955a22bc34c60e5edf1c
Pedro Fernandes <85568352+pedrofdnn@users.noreply.github.com>
2024-01-11 16:17:24 -0300

    criado componente do form de login

---
+++ app/Components/LoginForm.tsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { AccountCircle, Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface LoginFormProps {
+  showPassword: boolean;
+  handleClickShowPassword: () => void;
+  handleMouseDownPassword: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  //   função de ocultar a senha
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form className="box-form flex flex-col flex-wrap justify-evenly">
+        <div className="flex flex-col flex-wrap justify-evenly m-[5px]">
+          <Box
+            className="rounded-md p-1"
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              backgroundColor: "#CAD2C5 ",
+            }}
+          >
+            <AccountCircle sx={{ color: "action.active", mr: 1, my: 1 }} />
+
+            <TextField
+              sx={{ fontSize: 28, width: 305 }}
+              id="input-with-sx"
+              label="Usuário"
+              variant="standard"
+              color="success"
+              type="email"
+            />
+          </Box>
+        </div>
+
+        <div className="flex flex-col flex-wrap justify-evenly m-[5px]">
+          <Box
+            className="rounded-md p-1"
+            sx={{
+              backgroundColor: "#CAD2C5 ",
+            }}
+          >
+            <FormControl
+              sx={{ width: "34ch", marginLeft: 4 }}
+              variant="standard"
+              color="success"
+            >
+              <InputLabel htmlFor="standard-adornment-password">
+                Senha
+              </InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      sx={{ mr: -5, my: 1 }}
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </Box>
+        </div>
+
+        <br />
+        <div className="flex flex-col mx-2 items-stretch">
+          <Button
+            className="button-login mx-4"
+            variant="contained"
+            color="success"
+            type="submit"
+          >
+            LOGIN
+          </Button>
+          <br />
+          <Button
+            className="button-login"
+            variant="contained"
+            color="success"
+            type="submit"
+          >
+            REGISTRE-SE
+          </Button>
+        </div>
+
+        <div className=" container-stipe my-1">
+          <div className="stripe-left text-center"></div>
+          <div className="m-[1em] text-center ">ou</div>
+          <div className="stripe-right"></div>
+        </div>
+
+        <div className="flex flex-wrap justify-around items-stretch ">
+          <Button
+            className="button-login"
+            variant="contained"
+            color="primary"
+            startIcon={<FacebookIcon />}
+          >
+            Facebook
+          </Button>
+
+          <Button
+            className="button-login"
+            variant="contained"
+            color="secondary"
+            startIcon={<InstagramIcon />}
+          >
+            Instagram
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- app/Pages/LoginPage.tsx
+++ app/Pages/LoginPage.tsx
@@ -2,34 +2,12 @@
 import { useState } from "react";
 import Image from "next/image";
 import img1 from "../Assets/icon1.png";
-import {
-  Box,
-  Button,
-  FilledInput,
-  FormControl,
-  IconButton,
-  Input,
-  InputAdornment,
-  InputLabel,
-  TextField,
-} from "@mui/material";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import { AccountCircle, Visibility, VisibilityOff } from "@mui/icons-material";
+import LoginForm from "../Components/LoginForm";
 
-export default function LoginPage() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-
+export default function LoginPage({}) {
   return (
     <div className="container-start flex flex-row flex-wrap justify-evenly items-center content-center">
+      
       <div className="oxygen font-bold tirtle-login flex flex-col">
         <h1 className="tirtle-login text-4xl ">Bem Vindo</h1>
 
@@ -49,121 +27,7 @@ export default function LoginPage() {
           Loca Filmes
         </h1>
 
-        <form className="box-form flex flex-col flex-wrap justify-evenly">
-          <div className="flex flex-col flex-wrap justify-evenly m-[5px]">
-            <Box
-              className="rounded-md p-1"
-              sx={{
-                display: "flex",
-                alignItems: "flex-end",
-                backgroundColor: "#CAD2C5 ",
-              }}
-            >
-              <AccountCircle sx={{ color: "action.active", mr: 1, my: 1 }} />
-
-              <TextField
-                sx={{ fontSize: 28, width: 305 }}
-                id="input-with-sx"
-                label="Usuário"
-                variant="standard"
-                color="success"
-                type="email"
-              />
-            </Box>
-          </div>
-
-          <div className="flex flex-col flex-wrap justify-evenly m-[5px]">
-            <Box
-              className="rounded-md p-1"
-              sx={{
-                backgroundColor: "#CAD2C5 ",
-              }}
-            >
-              <FormControl
-                sx={{ width: "34ch", marginLeft: 4 }}
-                variant="standard"
-                color="success"
-              >
-                <InputLabel htmlFor="standard-adornment-password">
-                  Senha
-                </InputLabel>
-                <Input
-                  id="standard-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        sx={{ mr: -5, my: 1 }}
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </Box>
-
-            {/* <TextField
-              label="Senha"
-              className="rounded-t-md rounded-tr-md"
-              id="filled-basic"
-              variant="filled"
-              style={{ backgroundColor: "#CAD2C5 " }}
-              color="success"
-              type="password"
-            /> */}
-          </div>
-
-          <br />
-          <div className="flex flex-col mx-2 items-stretch">
-            <Button
-              className="button-login mx-4"
-              variant="contained"
-              color="success"
-              type="submit"
-            >
-              LOGIN
-            </Button>
-            <br />
-            <Button
-              className="button-login"
-              variant="contained"
-              color="success"
-              type="submit"
-            >
-              REGISTRE-SE
-            </Button>
-          </div>
-
-          <div className=" container-stipe my-1">
-            <div className="stripe-left text-center"></div>
-            <div className="m-[1em] text-center ">ou</div>
-            <div className="stripe-right"></div>
-          </div>
-
-          <div className="flex flex-wrap justify-around items-stretch ">
-            <Button
-              className="button-login"
-              variant="contained"
-              color="primary"
-              startIcon={<FacebookIcon />}
-            >
-              Facebook
-            </Button>
-
-            <Button
-              className="button-login"
-              variant="contained"
-              color="secondary"
-              startIcon={<InstagramIcon />}
-            >
-              Instagram
-            </Button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   );
